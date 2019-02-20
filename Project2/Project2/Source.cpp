e39bab7b106167331e9b94a2a93c23814bd343c1
Tendo96 <https://github.com/Tendo96/Tendo.git>
2019-02-20 15:41:20 +0900

    Project1

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,11 @@
+#ifndef TEST
+#include <iostream>
+#include "numbers.h"
+using namespace std;
+
+int main (int argc, char *argv [])
+{
+cout<<"hello world!";
+system ("pause");
+}
+#endif
---
+++ Project2/Project2/tests.cpp
@@ -0,0 +1,32 @@
+#ifdef TEST
+#define BOOST_TEST_MODULE tests
+#include <boost/test/included/unit_test.hpp>
+#include "numbers.h"
+BOOST_AUTO_TEST_SUITE( test_suite1 )
+BOOST_AUTO_TEST_CASE( test_case1 )
+{
+	
+	BOOST_TEST(string_from_int(0)==string("Ќоль"));
+	BOOST_TEST(string_from_int(1)==string("ќдин"));
+	BOOST_TEST(string_from_int(2)==string("два"));
+	BOOST_TEST(string_from_int(3)==string("три"));
+	BOOST_TEST(string_from_int(4)==string("четыре"));
+	BOOST_TEST(string_from_int(5)==string("п€ть"));
+	BOOST_TEST(string_from_int(6)==string("п€ть"));
+	BOOST_TEST(string_from_int(7)==string("шесть"));
+	BOOST_TEST(string_from_int(8)==string("семь"));
+	BOOST_TEST(string_from_int(9)==string("восемь"));
+	BOOST_TEST(string_from_int(10)==string("дев€ть"));
+	BOOST_TEST(string_from_int(11)==string("дес€ть"));
+	BOOST_TEST(string_from_int(12)==string("ќдиннадцать"));
+	BOOST_TEST(string_from_int(13)==string("двенадцать"));
+	BOOST_TEST(string_from_int(14)==string("тринадцать"));
+	BOOST_TEST(string_from_int(15)==string("четырнадцать"));
+	BOOST_TEST(string_from_int(16)==string("п€тнадцать"));
+	BOOST_TEST(string_from_int(17)==string("шестнадцать"));
+	BOOST_TEST(string_from_int(18)==string("семнадцать"));
+	BOOST_TEST(string_from_int(19)==string("восемнадцать"));
+	BOOST_TEST(string_from_int(20)==string("дев€тнадцать"));
+}
+BOOST_AUTO_TEST_SUITE_END()
+#endif
