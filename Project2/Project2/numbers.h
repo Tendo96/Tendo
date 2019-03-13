5ffed4b26f6f851af90b4d7321e566971cb6b78a
Tendo96 <https://github.com/Tendo96/Tendo.git>
2019-03-13 15:41:11 +0900

    upgraded 13.03

--- Project2/Project2/numbers.h
+++ Project2/Project2/numbers.h
@@ -4,13 +4,14 @@
 
 using namespace std;
 
-string string_from_int(int number)
+//string string_from_int(int number)
+string number_less_20_to_string(int number)
 {
 	string answer;
 	switch (number)
 	{
 	case 0:
-		answer="Ноль";
+		answer="ноль";
 			break;
 	case 1:
 		answer="один";
@@ -69,13 +70,24 @@ string string_from_int(int number)
 	case 19:
 		answer="девятнадцать";
 			break;
+			return answer;
 	}
+}
+	string number_less_100_to_string(int number)
+{
+	string answer;
+	if(number<20)
+	{
+		answer=number_less_20_to_string(number);
+	}
+	else
+	
 	if (number/10==2)
 	{
 	int a=number%10;
 	if (a==0)
 		answer="двадцать";
-	else answer="двадцать "+string_from_int(a);
+	else answer="двадцать "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==3)
@@ -83,7 +95,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="тридцать";
-	else answer="тридцать "+string_from_int(a);
+	else answer="тридцать "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==4)
@@ -91,7 +103,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="сорок";
-	else answer="сорок "+string_from_int(a);
+	else answer="сорок "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==5)
@@ -99,7 +111,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="пятьдесят";
-	else answer="пятьдесят "+string_from_int(a);
+	else answer="пятьдесят "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==6)
@@ -107,7 +119,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="шестьдесят";
-	else answer="шестьдесят "+string_from_int(a);
+	else answer="шестьдесят "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==7)
@@ -115,7 +127,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="семьдесят";
-	else answer="семьдесят "+string_from_int(a);
+	else answer="семьдесят "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==8)
@@ -123,7 +135,7 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="восемьдесят";
-	else answer="восемьдесят "+string_from_int(a);
+	else answer="восемьдесят "+number_less_20_to_string(a);
 	}
 	else
 		if (number/10==9)
@@ -131,8 +143,91 @@ string string_from_int(int number)
 	int a=number%10;
 	if (a==0)
 		answer="девяносто";
-	else answer="девяносто "+string_from_int(a);
+	else answer="девяносто "+number_less_20_to_string(a);
+	}
+	
+		return answer;
+	}
+	
+	string number_less_1000_to_string(int number)
+{
+	string answer;
+	if(number<100)
+	{
+		answer=number_less_100_to_string(number);
+	}
+		if (number/100==1)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="сто";
+	else answer="сто "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==2)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="двести";
+	else answer="двести "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==3)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="триста";
+	else answer="триста "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==4)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="четыреста";
+	else answer="четыреста "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==5)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="пятьсот";
+	else answer="пятьсот "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==6)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="шестьсот";
+	else answer="шестьсот "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==7)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="семьсот";
+	else answer="семьсот "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==8)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="восемьсот";
+	else answer="восемьсот "+number_less_100_to_string(a);
+	}
+	else
+		if (number/100==9)
+	{
+	int a=number%100;
+	if (a==0)
+		answer="девятьсот";
+	else answer="девятьсот "+number_less_100_to_string(a);
 	}
 	return answer;
+
 }
 #endif
--- Project2/Project2/tests.cpp
+++ Project2/Project2/tests.cpp
@@ -2,43 +2,59 @@
 #define BOOST_TEST_MODULE tests
 #include <boost/test/included/unit_test.hpp>
 #include "numbers.h"
-#include "100.h"
 BOOST_AUTO_TEST_SUITE( test_suite1 )
 BOOST_AUTO_TEST_CASE( test_case1 )
 {
-	BOOST_TEST(string_from_int(0)==string("ноль"));
-	BOOST_TEST(string_from_int(1)==string("один"));
-	BOOST_TEST(string_from_int(2)==string("два"));
-	BOOST_TEST(string_from_int(3)==string("три"));
-	BOOST_TEST(string_from_int(4)==string("четыре"));
-	BOOST_TEST(string_from_int(5)==string("пять"));
-	BOOST_TEST(string_from_int(6)==string("шесть"));
-	BOOST_TEST(string_from_int(7)==string("семь"));
-	BOOST_TEST(string_from_int(8)==string("восемь"));
-	BOOST_TEST(string_from_int(9)==string("девять"));
-	BOOST_TEST(string_from_int(10)==string("десять"));
-	BOOST_TEST(string_from_int(11)==string("одиннадцать"));
-	BOOST_TEST(string_from_int(12)==string("двенадцать"));
-	BOOST_TEST(string_from_int(13)==string("тринадцать"));
-	BOOST_TEST(string_from_int(14)==string("четырнадцать"));
-	BOOST_TEST(string_from_int(15)==string("пятнадцать"));
-	BOOST_TEST(string_from_int(16)==string("шестнадцать"));
-	BOOST_TEST(string_from_int(17)==string("семнадцать"));
-	BOOST_TEST(string_from_int(18)==string("восемнадцать"));
-	BOOST_TEST(string_from_int(19)==string("девятнадцать"));
+	BOOST_TEST(number_less_20_to_string(0)==string("ноль"));
+	BOOST_TEST(number_less_20_to_string(1)==string("один"));
+	BOOST_TEST(number_less_20_to_string(2)==string("два"));
+	BOOST_TEST(number_less_20_to_string(3)==string("три"));
+	BOOST_TEST(number_less_20_to_string(4)==string("четыре"));
+	BOOST_TEST(number_less_20_to_string(5)==string("пять"));
+	BOOST_TEST(number_less_20_to_string(6)==string("шесть"));
+	BOOST_TEST(number_less_20_to_string(7)==string("семь"));
+	BOOST_TEST(number_less_20_to_string(8)==string("восемь"));
+	BOOST_TEST(number_less_20_to_string(9)==string("девять"));
+	BOOST_TEST(number_less_20_to_string(10)==string("десять"));
+	BOOST_TEST(number_less_20_to_string(11)==string("одиннадцать"));
+	BOOST_TEST(number_less_20_to_string(12)==string("двенадцать"));
+	BOOST_TEST(number_less_20_to_string(13)==string("тринадцать"));
+	BOOST_TEST(number_less_20_to_string(14)==string("четырнадцать"));
+	BOOST_TEST(number_less_20_to_string(15)==string("пятнадцать"));
+	BOOST_TEST(number_less_20_to_string(16)==string("шестнадцать"));
+	BOOST_TEST(number_less_20_to_string(17)==string("семнадцать"));
+	BOOST_TEST(number_less_20_to_string(18)==string("восемнадцать"));
+	BOOST_TEST(number_less_20_to_string(19)==string("девятнадцать"));
+}
+BOOST_AUTO_TEST_CASE( test_case2 )
+{
+	BOOST_TEST(number_less_100_to_string(11)==string("одиннадцать"));
+	BOOST_TEST(number_less_100_to_string(30)==string("тридцать"));
+	BOOST_TEST(number_less_100_to_string(45)==string("сорок пять"));
+	BOOST_TEST(number_less_100_to_string(59)==string("пятьдесят девять"));
+	BOOST_TEST(number_less_100_to_string(62)==string("шестьдесят два"));
+	BOOST_TEST(number_less_100_to_string(74)==string("семьдесят четыре"));
+	BOOST_TEST(number_less_100_to_string(88)==string("восемьдесят восемь"));
+	BOOST_TEST(number_less_100_to_string(95)==string("девяносто пять"));
+	BOOST_TEST(number_less_100_to_string(93)==string("девяносто три"));
 
-
-	BOOST_TEST(string_from_int(21)==string("двадцать один"));
-	BOOST_TEST(string_from_int(30)==string("тридцать"));
-	BOOST_TEST(string_from_int(45)==string("сорок пять"));
-	BOOST_TEST(string_from_int(59)==string("пятьдесят девять"));
-	BOOST_TEST(string_from_int(62)==string("шестьдесят два"));
-	BOOST_TEST(string_from_int(74)==string("семьдесят четыре"));
-	BOOST_TEST(string_from_int(88)==string("восемьдесят восемь"));
-	BOOST_TEST(string_from_int(95)==string("девяносто пять"));
-	BOOST_TEST(string_from_int(10)==string("десять"));
-	BOOST_TEST(string_from_int(93)==string("девяносто три"));
-
+}
+BOOST_AUTO_TEST_CASE(test_case3)
+{
+	BOOST_TEST(number_less_1000_to_string(151)==string("сто пятьдесят один"));
+	BOOST_TEST(number_less_1000_to_string(215)==string("двести пятнадцать"));
+	BOOST_TEST(number_less_1000_to_string(300)==string("триста"));
+	BOOST_TEST(number_less_1000_to_string(456)==string("четыреста пятьдесят шесть"));
+	BOOST_TEST(number_less_1000_to_string(599)==string("пятьсот девяносто девять"));
+	BOOST_TEST(number_less_1000_to_string(620)==string("шестьсот двадцать"));
+	BOOST_TEST(number_less_1000_to_string(742)==string("семьсот сорок два"));
+	BOOST_TEST(number_less_1000_to_string(880)==string("восемьсот восемьдесят"));
+	BOOST_TEST(number_less_1000_to_string(950)==string("девятьсот пятьдесят"));
+	BOOST_TEST(number_less_1000_to_string(53)==string("пятьдесят три"));
+	BOOST_TEST(number_less_1000_to_string(0)==string("ноль"));
+	BOOST_TEST(number_less_1000_to_string(100)==string("сто"));
+	BOOST_TEST(number_less_1000_to_string(105)==string("сто пять"));
+	BOOST_TEST(number_less_1000_to_string(393)==string("триста девяносто три"));
 }
 BOOST_AUTO_TEST_SUITE_END()
 #endif
