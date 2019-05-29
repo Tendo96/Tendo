676700470d600a783ff452c94cbd259ac5881d5d
Tendo96 <https://github.com/Tendo96/Tendo.git>
2019-05-29 14:51:17 +0900

    final shtrix lab1

--- Project2/Project2/numbers.h
+++ Project2/Project2/numbers.h
@@ -10,51 +10,21 @@ string number_less_20_to_string(int number)
 	string answer;
 	switch (number)
 	{
-	case 0:
-		answer="ноль";
-			break;
-	case 1:
-		answer="один";
-			break;
-	case 2:
-		answer="два";
-			break;
-	case 3:
-		answer="три";
-			break;
-	case 4:
-		answer="четыре";
-			break;
-	case 5:
-		answer="пять";
-			break;
-	case 6:
-		answer="шесть";
-			break;
-	case 7:
-		answer="семь";
-			break;
-	case 8:
-		answer="восемь";
-			break;
-	case 9:
-		answer="девять";
-			break;
-	case 10:
-		answer="десять";
-			break;
-	case 11:
-		answer="одиннадцать";
-			break;
-	case 12:
-		answer="двенадцать";
-			break;
-	case 13:
-		answer="тринадцать";
-			break;
-	case 14:
-		answer="четырнадцать";
-			break;
+	case 0:answer="ноль";break;
+	case 1:answer="один";break;
+	case 2:answer="два";break;
+	case 3:answer="три";break;
+	case 4:answer="четыре";break;
+	case 5:answer="пять";break;
+	case 6:answer="шесть";break;
+	case 7:answer="семь";break;
+	case 8:answer="восемь";break;
+	case 9:answer="девять";break;
+	case 10:answer="десять";break;
+	case 11:answer="одиннадцать";break;
+	case 12:answer="двенадцать";break;
+	case 13:answer="тринадцать";break;
+	case 14:answer="четырнадцать";break;
 	case 15:
 		answer="пятнадцать";
 			break;
@@ -70,8 +40,8 @@ string number_less_20_to_string(int number)
 	case 19:
 		answer="девятнадцать";
 			break;
-			return answer;
 	}
+	return answer;
 }
 	string number_less_100_to_string(int number)
 {
